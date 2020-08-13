f02c64e0b125a0bb1be964a97a58b06e63248b66
Ubuntu <ubuntu@ip-172-31-50-135.ec2.internal>
2020-08-13 17:48:58 +0000

    combined codebuild buckets under single supporting-infrastructure stack

--- create-in-order.sh
+++ create-in-order.sh
@@ -6,7 +6,7 @@ set -ex # exit if command fails and display commands https://www.peterbe.com/plo
 aws cloudformation deploy --stack-name=cloud-formation-templates-bucket-us-east-1 --parameter-overrides TheBucketName=cf-templates--us-east-1--curi-bio--prod EnableServerAccessLogging=false --template-file=aws/templates/s3/generic-bucket.yaml
 
 # package and deploy the S3 bucket to hold Server Access Logs
-aws cloudformation package --template-file aws/modl-prod/s3-server-logs-bucket.yaml --output-template-file s3-server-logs-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
+aws cloudformation package --template-file aws/modl-prod/s3/s3-server-logs-bucket.yaml --output-template-file s3-server-logs-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
 aws cloudformation deploy --stack-name=s3-server-logs-bucket-us-east-1 --template-file=s3-server-logs-bucket.packaged.yaml --parameter-overrides Stage=prod
 
 # Enable server access logging for the CloudFormation templates bucket
@@ -14,20 +14,19 @@ aws cloudformation deploy --stack-name=cloud-formation-templates-bucket-us-east-
 
 #### S3 Buckets ####
 
-
-# Build artifacts and Build Resources
-aws cloudformation package --template-file aws/model-prod/s3/build-artifacts-bucket.yaml --output-template-file build-artifacts-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
-aws cloudformation deploy --stack-name=build-artifacts-bucket --template-file=build-artifacts-bucket.packaged.yaml --parameter-overrides Stage=prod
-
-aws cloudformation package --template-file aws/model-prod/build-resources.yaml --output-template-file build-resources.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
-aws cloudformation deploy --stack-name=build-resources --template-file=build-resources.packaged.yaml --parameter-overrides Stage=prod
-
 # Static Website Hosting
-aws cloudformation package --template-file aws/model-prod/s3/general-downloads-bucket.yaml --output-template-file general-downloads-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
+aws cloudformation package --template-file aws/modl-prod/s3/general-downloads-bucket.yaml --output-template-file general-downloads-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
 aws cloudformation deploy --stack-name=general-downloads-bucket --template-file=general-downloads-bucket.packaged.yaml
 
-aws cloudformation package --template-file aws/model-prod/s3/software-downloads-bucket.yaml --output-template-file software-downloads-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
+aws cloudformation package --template-file aws/modl-prod/s3/software-downloads-bucket.yaml --output-template-file software-downloads-bucket.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
 aws cloudformation deploy --stack-name=software-downloads-bucket --template-file=software-downloads-bucket.packaged.yaml
+
+# General Build resources (S3 Buckets and SSM Parameters)
+## Note: the SSM Parameters need to be changed from String to SecureString and have the values entered (CloudFormation does not yet support SecureString)
+aws cloudformation package --template-file aws/modl-prod/codebuild-supporting-infrastructure.yaml --output-template-file codebuild-supporting-infrastructure.packaged.yaml --s3-bucket cf-templates--us-east-1--curi-bio--prod
+aws cloudformation deploy --stack-name=codebuild-supporting-infrastructure --template-file=codebuild-supporting-infrastructure.packaged.yaml --parameter-overrides Stage=prod
+
+
 
 # clean up
 rm *.packaged.yaml
