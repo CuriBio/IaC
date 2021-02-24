bcb2dd9ca24b03c64733fcaac2782d68017b20c3
Jason Person <jrperson@gmail.com>
2021-02-24 13:17:45 -0800

    * install terragrunt in github workflow * include terragrunt in install-terraform.sh script

--- .bash_scripts/install-terraform.sh
+++ .bash_scripts/install-terraform.sh
@@ -8,3 +8,9 @@ sudo wget https://releases.hashicorp.com/terraform/0.14.7/terraform_0.14.7_linux
 sudo unzip terraform_0.14.7_linux_amd64.zip –d /usr/local/bin
 
 terraform –v
+
+
+# install terragrunt v0.28.7
+sudo wget -q -O /usr/local/bin/terragrunt "https://github.com/gruntwork-io/terragrunt/releases/download/v0.28.7/terragrunt_linux_amd64"
+sudo chmod +x /usr/local/bin/terragrunt
+terragrunt -v
