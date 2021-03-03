96ca0cc5ef7c16b3513b19aba59fc0f57aec69af
Jason Person <jrperson@gmail.com>
2021-03-03 12:55:34 -0800

    prefix with workspace name

--- infra/environments/test/service/main.tf
+++ infra/environments/test/service/main.tf
@@ -13,7 +13,7 @@ terraform {
 
 module "ecr_image" {
   source  = "../../../modules/aws/ecr_image_build_push"
-  image_name = "hello_world"
+  image_name = "${terraform.workspace}_hello_world"
   image_src = "../../../../src/lambdas/hello_world"
   role_arn     = "arn:aws:iam::077346344852:role/terraform_deploy_role"
 }
@@ -21,6 +21,6 @@ module "ecr_image" {
 module "data_processor" {
   depends_on = [module.ecr_image]
   source                = "../../../modules/curi/data_processor"
-  data_processor_bucket = "curi-test-data"
+  data_processor_bucket = "${terraform.workspace}-curi-test-data"
   ecr_repository_url    = module.ecr_image.ecr_repository_url
 }
--- infra/modules/aws/ecr_image_build_push/push.sh
+++ infra/modules/aws/ecr_image_build_push/push.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-set -ex
-
 eval $(/usr/local/bin/aws sts assume-role --role-arn $ROLE_ARN --role-session-name terraform | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
 unset AWS_PROFILE
 /usr/local/bin/aws ecr get-login-password --region us-east-1 | docker login --password-stdin --username AWS $ECR_REPO
