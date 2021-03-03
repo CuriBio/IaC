0de0fa551e81baf8f6616e6b3bf7ef8f7cd48101
Jason Person <jrperson@gmail.com>
2021-03-03 15:36:12 -0800

    logging

--- infra/modules/aws/ecr_image_build_push/push.sh
+++ infra/modules/aws/ecr_image_build_push/push.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 set -ex
 
+aws --version
 SESSION=$(aws sts assume-role --role-arn $ROLE_ARN --role-session-name terraform)
+echo $SESSION
 unset AWS_PROFILE
+
 unset AWS_ACCESS_KEY_ID
-export AWS_ACCESS_KEY_ID=$(echo $SESSION | jq -r .Credentials.AccessKeyId)
+export AWS_ACCESS_KEY_ID=$(echo $SESSION | jq -r ".Credentials.AccessKeyId")
 
 unset AWS_SECRET_ACCESS_KEY
-export AWS_SECRET_ACCESS_KEY=$(echo $SESSION | jq -r .Credentials.SecretAccessKey)
+export AWS_SECRET_ACCESS_KEY=$(echo $SESSION | jq -r ".Credentials.SecretAccessKey")
 
 unset AWS_SESSION_TOKEN
-export AWS_SESSION_TOKEN=$(echo $SESSION | jq -r .Credentials.SessionToken)
-env
-aws ecr get-login-password --region us-east-1 | docker login --password-stdin --username AWS $ECR_REPO
+export AWS_SESSION_TOKEN=$(echo $SESSION | jq -r ".Credentials.SessionToken")
 
+aws ecr get-login-password --region us-east-1 | docker login --password-stdin --username AWS $ECR_REPO
 docker push $ECR_REPO:"$TAG"
