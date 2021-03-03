efe77cbb7d05bd54c6d6ce3c5133f379a82ed7ff
Jason Person <jrperson@gmail.com>
2021-03-03 14:11:23 -0800

    credentials

--- infra/modules/aws/ecr_image_build_push/push.sh
+++ infra/modules/aws/ecr_image_build_push/push.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 set -ex
 
-eval $(/usr/local/bin/aws sts assume-role --role-arn $ROLE_ARN --role-session-name terraform | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
+SESSION=$(/usr/local/bin/aws sts assume-role --role-arn $ROLE_ARN --role-session-name terraform)
 unset AWS_PROFILE
+unset AWS_ACCESS_KEY_ID
+export AWS_ACCESS_KEY_ID=$(echo $SESSION | jq -r .Credentials.AccessKeyId)
+
+unset AWS_SECRET_ACCESS_KEY
+export AWS_SECRET_ACCESS_KEY=$(echo $SESSION | jq -r .Credentials.SecretAccessKey)
+
+unset AWS_SESSION_TOKEN
+export AWS_SESSION_TOKEN=$(echo $SESSION | jq -r .Credentials.SessionToken)
 env
 /usr/local/bin/aws ecr get-login-password --region us-east-1 | docker login --password-stdin --username AWS $ECR_REPO
 
