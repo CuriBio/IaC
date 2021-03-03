24ffea16257c1df45ebadbce0ec2576d7a909288
Jason Person <jrperson@gmail.com>
2021-03-03 12:12:40 -0800

    build lambda docker image with null_resource

--- .github/scripts/tf-bootstrap-env.sh
+++ .github/scripts/tf-bootstrap-env.sh
@@ -48,48 +48,23 @@ echo "$INFRADIR/$ENV_TF"
 # terragrunt init and select workspace if in test env
 if [[ $ENV_TF = 'test' ]]; then
   # ECR needs to be created first so docker image can be pushed before the lambda is provisioned
-  (cd $INFRADIR/$ENV_TF/ecr && terragrunt init --terragrunt-non-interactive)
-  (cd $INFRADIR/$ENV_TF/ecr && (terragrunt workspace select $WORKSPACE || terragrunt workspace new $WORKSPACE))
-  (cd $INFRADIR/$ENV_TF/ecr && terragrunt apply --terragrunt-non-interactive --auto-approve)
+  (cd $INFRADIR/$ENV_TF && terragrunt run-all init --terragrunt-non-interactive)
+  (cd $INFRADIR/$ENV_TF && (terragrunt run-all workspace select $WORKSPACE || terragrunt run-all workspace new $WORKSPACE))
 
-  (cd $INFRADIR/$ENV_TF/service && terragrunt init --terragrunt-non-interactive)
-  (cd $INFRADIR/$ENV_TF/service && (terragrunt workspace select $WORKSPACE || terragrunt workspace new $WORKSPACE))
-fi
-
-
-if [[ $DESTROY = true ]]; then
-  if [[ $ENV_TF = 'prod' ]]; then
-    echo "Destroy can't be used for prod environment"
-    exit 1
+  if [[ $DESTROY = true ]]; then
+      (cd $INFRADIR/$ENV_TF && terragrunt run-all destroy --terragrunt-non-interactive --auto-approve)
+      (cd $INFRADIR/$ENV_TF && terragrunt run-all workspace select default)
+      (cd $INFRADIR/$ENV_TF && terragrunt run-all workspace delete $WORKSPACE)
+      exit 0
   fi
-
-  (cd $INFRADIR/$ENV_TF && terragrunt run-all destroy --terragrunt-non-interactive --auto-approve)
-  (cd $INFRADIR/$ENV_TF && terragrunt run-all workspace select default)
-  (cd $INFRADIR/$ENV_TF && terragrunt run-all workspace delete $WORKSPACE)
-  exit 0
 fi
-
-
-# get ecr repo url and login
-role_arn=$(cd $INFRADIR/$ENV_TF/ecr && terragrunt output --raw arn)
-ecr_repo=$(cd $INFRADIR/$ENV_TF/ecr && terragrunt output --raw ecr_repository_url)
-
-# docker login needs the assumed role to push images to ecr
-eval $(aws sts assume-role --role-arn $role_arn --role-session-name terraform | jq -r '.Credentials | "export AWS_ACCESS_KEY_ID=\(.AccessKeyId)\nexport AWS_SECRET_ACCESS_KEY=\(.SecretAccessKey)\nexport AWS_SESSION_TOKEN=\(.SessionToken)\n"')
-aws ecr get-login-password --region us-east-1 | docker login --password-stdin --username AWS $ecr_repo
-unset AWS_SECRET_ACCESS_KEY AWS_ACCESS_KEY_ID AWS_SESSION_TOKEN
-
-# build images, push docker images to ecr
-(cd $SRCDIR/lambdas/hello_world && ECR_REPO=$ecr_repo make build)
-(cd $SRCDIR/lambdas/hello_world && ECR_REPO=$ecr_repo make tag)
-(cd $SRCDIR/lambdas/hello_world && ECR_REPO=$ecr_repo make push)
 
 # provision services
-plan_output=$(cd $INFRADIR/$ENV_TF/service && terragrunt plan --terragrunt-non-interactive)
+plan_output=$(cd $INFRADIR/$ENV_TF && terragrunt run-all plan --terragrunt-non-interactive)
 echo "::set-output name=plan_output::$plan_output"
 
 if [[ $APPLY_PLAN = true ]]; then
-  (cd $INFRADIR/$ENV_TF/service && terragrunt apply --terragrunt-non-interactive --auto-approve)
+  (cd $INFRADIR/$ENV_TF && terragrunt run-all apply --terragrunt-non-interactive --auto-approve)
 fi
 
 #echo $plan_output
