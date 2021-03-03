b44cd4451da7c727856fdea7c23055eb7f4ef402
Jason Person <jrperson@gmail.com>
2021-03-03 13:04:39 -0800

    image tag

--- infra/modules/aws/ecr_image_build_push/main.tf
+++ infra/modules/aws/ecr_image_build_push/main.tf
@@ -24,6 +24,8 @@ resource "null_resource" "docker_build" {
     environment = {
       ECR_REPO = aws_ecr_repository.ecr.repository_url
       ROLE_ARN = var.role_arn
+      IMAGE    = var.image_name
+      TAG      = "latest"
     }
   }
 }
