5a0272e8a500296e9fb516610aa331571a89f682
Jason Person <jrperson@gmail.com>
2021-03-03 16:11:12 -0800

    build lambda docker image with null_resource

--- infra/environments/test/ecr/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "ecr_repository_url" {
-  value       = aws_ecr_repository.ecr.repository_url
-  description = "ECR repository url"
-}
-
-output "arn" {
-  value       = var.role_arn
-  description = "role arn"
-}
--- infra/environments/test/ecr/variables.tf
+++
@@ -1,5 +0,0 @@
-variable "role_arn" {
-  type        = string
-  default     = "arn:aws:iam::077346344852:role/terraform_deploy_role"
-  description = "role arn for deploying"
-}
