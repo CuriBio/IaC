15da2ed2b2830bed1087259c3a342677034928e1
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-03-23 13:24:57 +0000

    added test for confirming bucket has public access

--- tests/test_downloads_bucket.py
+++ tests/test_downloads_bucket.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from typing import Any
+from typing import Dict
 import uuid
 
 import boto3
 import pytest
+import requests
 
 
 @pytest.fixture(scope="function", name="downloads_bucket_name")
@@ -13,16 +16,12 @@ def fixture_downloads_bucket_name(deployment_tier):
     return f"downloads.curibio{suffix}.com"
 
 
-def test_When_admin_account_assumes_marketing_role__Then_an_object_can_be_created_and_deleted_in_the_downloads_bucket(
-    tf_workspace_name, deployment_tier, deployment_aws_account_id, downloads_bucket_name
-):
-    assert isinstance(tf_workspace_name, str)
-    print(f"Workspace name in pytest: {tf_workspace_name}")  # allow-print
-    print(f"Determined deployment tier: {deployment_tier}")  # allow-print
-
+def _create_generic_object(
+    account_id: str, downloads_bucket_name: str
+) -> Dict[str, Any]:
     client = boto3.client("sts")
     assumed_role_object = client.assume_role(
-        RoleArn=f"arn:aws:iam::{deployment_aws_account_id}:role/s3_downloads_role",
+        RoleArn=f"arn:aws:iam::{account_id}:role/s3_downloads_role",
         RoleSessionName="a-role-session-name",
     )
     credentials = assumed_role_object["Credentials"]
@@ -37,7 +36,44 @@ def test_When_admin_account_assumes_marketing_role__Then_an_object_can_be_create
     obj_contents = str(uuid.uuid4())
 
     created_object = bucket.put_object(Body=obj_contents, Key=obj_key)
-    assert created_object.key == obj_key
+    return {"object": created_object, "contents": obj_contents, "key": obj_key}
+
+
+@pytest.fixture(scope="function", name="Given_an_object_is_in_the_downloads_bucket")
+def fixture_Given_an_object_is_in_the_downloads_bucket(
+    deployment_aws_account_id, downloads_bucket_name
+):
+    object_properties = _create_generic_object(
+        deployment_aws_account_id, downloads_bucket_name
+    )
+    yield object_properties
+    created_object = object_properties["object"]
+    # since this is a versioned bucket, to completely delete the object, the version ID needs to be specified
+    created_object.delete(VersionId=created_object.version_id)
+
+
+def test_When_admin_account_assumes_marketing_role__Then_an_object_can_be_created_and_deleted_in_the_downloads_bucket(
+    tf_workspace_name, deployment_tier, deployment_aws_account_id, downloads_bucket_name
+):
+    assert isinstance(tf_workspace_name, str)
+    print(f"Workspace name in pytest: {tf_workspace_name}")  # allow-print
+    print(f"Determined deployment tier: {deployment_tier}")  # allow-print
+    object_properties = _create_generic_object(
+        deployment_aws_account_id, downloads_bucket_name
+    )
+    created_object = object_properties["object"]
+    expected_key = object_properties["key"]
+    assert created_object.key == expected_key
 
     # since this is a versioned bucket, to completely delete the object, the version ID needs to be specified
     created_object.delete(VersionId=created_object.version_id)
+
+
+def test_Given_an_object_is_in_the_downloads_bucket__When_a_generic_http_request_is_sent_for_the_object__Then_it_can_be_accessed_since_the_bucket_is_configured_for_public_access(
+    downloads_bucket_name, Given_an_object_is_in_the_downloads_bucket
+):
+    object_properties = Given_an_object_is_in_the_downloads_bucket
+    object_key = object_properties["object"].key
+    url = f"https://{downloads_bucket_name}/{object_key}"
+    r = requests.get(url)
+    assert r.text == object_properties["contents"]
