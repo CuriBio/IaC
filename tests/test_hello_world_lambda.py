6891c0f707aa2770cbca502c23c590673438a61a
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-03-25 11:43:33 +0000

    attempting to see what response looks like in pull request context for lambda

---
+++ tests/test_hello_world_lambda.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+import os
+
+import boto3
+import hcl2
+
+from .fixtures import PATH_TO_INFRA_DIR
+
+
+def When_the_function_is_invoked__Then_it_returns_a_response(
+    deployment_tier, tf_workspace_name, deployment_aws_account_id
+):
+    with open(
+        os.path.join(
+            PATH_TO_INFRA_DIR, "environments", deployment_tier, "terraform.tfvars"
+        ),
+        "r",
+    ) as in_file:
+        tf_info = hcl2.load(in_file)
+    lambda_function_name = tf_info["function_name"][0]
+    lambda_function_name = f"{tf_workspace_name}-{lambda_function_name}"
+    lambda_arn = f"arn:aws:lambda:us-east-1:{deployment_aws_account_id}:function:{lambda_function_name}"
+    lambda_client = boto3.client("lambda")
+    response = lambda_client.invoke(
+        FunctionName=lambda_arn, InvocationType="RequestResponse",
+    )
+    print(response)  # allow-print
