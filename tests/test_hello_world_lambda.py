d4e787faaa6e56b05a77a9882b6079624404db94
Eli Fine <ubuntu@ip-172-31-89-59.ec2.internal>
2021-03-26 17:28:16 +0000

    patching case where CodeArtifactUserPendingException occurs

--- tests/test_hello_world_lambda.py
+++ tests/test_hello_world_lambda.py
@@ -1,10 +1,31 @@
 # -*- coding: utf-8 -*-
 import json
+import logging
 import os
+import time
 
+import botocore
 import hcl2
 
 from .fixtures import PATH_TO_INFRA_DIR
+
+logger = logging.getLogger(__name__)
+
+
+def _invoke_lambda(lambda_client, *args, **kwargs):
+    try:
+        return lambda_client.invoke(*args, **kwargs)
+    except botocore.exceptions.ClientError as e:
+        # Eli (3/26/21): Sporadic errors occuring in CI after a terraform apply. There doesn't appear to be standard documentation on how to handle this, so just trying to sleep and run it again
+        # botocore.exceptions.ClientError: An error occurred (CodeArtifactUserPendingException) when calling the Invoke operation (reached max retries: 4): INFO: Lambda is initializing your function. It will be ready to invoke shortly.
+        if "CodeArtifactUserPendingException" in str(e):
+            logger.warning(
+                "A CodeArtifactUserPendingException occurred, sleeping for 15 seconds and trying again: %s",
+                e,
+            )
+            time.sleep(15)
+            return lambda_client.invoke(*args, **kwargs)
+        raise e
 
 
 def When_the_function_is_invoked__Then_it_returns_the_expected_response_payload(
@@ -21,8 +42,8 @@ def When_the_function_is_invoked__Then_it_returns_the_expected_response_payload(
     lambda_function_name = f"{tf_workspace_name}-{lambda_function_name}"
     lambda_arn = f"arn:aws:lambda:us-east-1:{deployment_aws_account_id}:function:{lambda_function_name}"
     lambda_client = boto3_test_session.client("lambda", region_name="us-east-1")
-    response = lambda_client.invoke(
-        FunctionName=lambda_arn, InvocationType="RequestResponse",
+    response = _invoke_lambda(
+        lambda_client, FunctionName=lambda_arn, InvocationType="RequestResponse",
     )
     assert response["StatusCode"] == 200
     extracted_payload = json.loads(response["Payload"].read().decode("utf-8"))
