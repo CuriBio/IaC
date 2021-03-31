213fdbf973b708929ee314f81b4919d9f6103ff3
Jason Person <jrperson@gmail.com>
2021-03-31 16:46:41 -0700

    formatting

--- infra/modules/curi/jupyter_sdk/variables.tf
+++ infra/modules/curi/jupyter_sdk/variables.tf
@@ -1,19 +1,19 @@
 variable "hosted_zone" {
   description = "Hosted zone"
-  type = string
+  type        = string
 }
 
 variable "version_tag" {
   description = "Github tag"
-  type = string
+  type        = string
 }
 
 variable "hosted_zone_id" {
   description = "Hosted zone id"
-  type = string
+  type        = string
 }
 
 variable "ssl_cert_arn" {
   description = "SSL cert arn for hosted zone"
-  type = string
+  type        = string
 }
