0882006feeab0470e468155844aa9bc3649f837c
Jason Person <jrperson@gmail.com>
2021-09-01 08:13:12 -0700

    update tfvars for modl and prod, set # of ecs tasks to 0 for now

--- infra/modules/curi/ecs_task/main.tf
+++ infra/modules/curi/ecs_task/main.tf
@@ -143,7 +143,7 @@ resource "aws_ecs_service" "aws-ecs-service" {
 
   launch_type          = "FARGATE"
   scheduling_strategy  = "REPLICA"
-  desired_count        = 1
+  desired_count        = 0
 
   force_new_deployment = true
 
