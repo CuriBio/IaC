1ee428141202eaaccf7577cc66a4ceae37dd3bdc
Luci Pak <pak.luci.13@gmail.com>
2021-09-07 13:08:25 -0700

    removed unused variables for aurora

--- infra/modules/curi/aurora_db/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "role_arn" {
-  type        = string
-  description = "role arn w/permission to assume role"
-}
-
-variable "image_name" {
-  type        = string
-  description = "docker image name"
-}
