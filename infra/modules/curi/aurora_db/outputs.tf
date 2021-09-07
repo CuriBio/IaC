1ebe75eb864dd609d8f7dbdea6ad9dee8d847f30
Luci Pak <pak.luci.13@gmail.com>
2021-09-07 12:44:12 -0700

    added aurora module under modules/curi/aurora_db

---
+++ infra/modules/curi/aurora_db/outputs.tf
@@ -0,0 +1,14 @@
+output "rds_cluster_master_password" {
+  description = "aurora database master password"
+  value       = module.aurora_db.master_password
+}
+
+output "rds_cluster_master_username" {
+  description = "aurora database master username"
+  value       = module.aurora_db.master_username
+}
+
+output "security_group_id" {
+  description = "aurora database security group id"
+  value       = module.aurora_db.security_group_id
+}
---
+++ infra/modules/curi/aurora_db/variables.tf
@@ -0,0 +1,9 @@
+variable "role_arn" {
+  type        = string
+  description = "role arn w/permission to assume role"
+}
+
+variable "image_name" {
+  type        = string
+  description = "docker image name"
+}
