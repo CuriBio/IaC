803b4c5ad26a3d8b9d4320029e34f8d98162f9f3
Luci Pak <pak.luci.13@gmail.com>
2021-09-07 13:43:26 -0700

    fixed pre-commit hook

--- infra/modules/curi/aurora_db/main.tf
+++ infra/modules/curi/aurora_db/main.tf
@@ -1,47 +1,38 @@
 locals {
-  name   = "${terraform.workspace}-mantarray_db"
+  name = "${terraform.workspace}-mantarray_db"
   region = "us_east_1"
   tags = {
     Application = "mantarray_db"
     Environment = "${terraform.workspace}"
   }
 }
-
 resource "aws_db_parameter_group" "parameter_group" {
-  name        = "${local.name}-parameter-group"
-  family      = "aurora-mysql5.7"
+  name = "${local.name}-parameter-group"
+  family = "aurora-mysql5.7"
   description = "${local.name}-parameter-group"
-  tags        = local.tags
+  tags = local.tags
 }
-
 resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
-  name        = "${local.name}-cluster-parameter-group"
-  family      = "aurora-mysql5.7"
+  name = "${local.name}-cluster-parameter-group"
+  family = "aurora-mysql5.7"
   description = "${local.name}-cluster-parameter-group"
-  tags        = local.tags
+  tags = local.tags
 }
-
 # Providing a reference to our default VPC
 resource "aws_default_vpc" "default_vpc" {
 }
-
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
   availability_zone = "${local.region}a"
 }
-
 resource "aws_default_subnet" "default_subnet_b" {
   availability_zone = "${local.region}b"
 }
-
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "${local.region}c"
 }
-
-
 module "aurora_db" {
-
-  source  = "terraform-aws-modules/rds-aurora/aws"
+  source = "terraform-aws-modules/rds-aurora/aws"
   version = "~> 3.0"
 
   name = "${local.name}"
@@ -54,7 +45,7 @@ module "aurora_db" {
   subnets = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
   allowed_cidr_blocks = aws_default_vpc.default_vpc.cidr_block
 
-  apply_immediately   = true
+  apply_immediately = true
   skip_final_snapshot = true
 
   db_parameter_group_name = aws_db_parameter_group.parameter_group.id
