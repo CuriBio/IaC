2f34c2a9796a833bebacaec409fef3a308a002a2
Luci Pak <pak.luci.13@gmail.com>
2021-09-08 13:15:53 -0700

    testing local-exec provisioner to run sql file

--- infra/modules/curi/aurora_db/main.tf
+++ infra/modules/curi/aurora_db/main.tf
@@ -21,6 +21,9 @@ resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
   description = "${local.name}-cluster-parameter-group"
   tags        = local.tags
 }
+resource "random_password" "master_password" {
+  length = 10
+}
 # Data sources to get VPC and subnets
 data "aws_vpc" "default" {
   default = true
@@ -42,10 +45,19 @@ module "db" {
   replica_count     = 1
   apply_immediately = true
 
-  username = var.master_username
+  username               = var.master_username
+  password               = random_password.master_password.result
+  create_random_password = false
 
   db_parameter_group_name         = aws_db_parameter_group.parameter_group.id
   db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_parameter_group.id
 
   tags = local.tags
+}
+
+resource "null_resource" "setup_db" {
+  depends_on = [module.db] #wait for the db to be ready
+  provisioner "local-exec" {
+    command = "mysql -u ${module.db.username} -p ${random_password.master_password.result} -h ${aws_rds_cluster_instance.this.endpoint} < schema.sql"
+  }
 }
