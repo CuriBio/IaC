cc017e16e1174208ab02ee942b308236b0fd7b11
Tanner Peterson <tannermp23@hotmail.com>
2021-09-08 16:21:04 -0700

    added stage

--- infra/modules/curi/api_gateway/main.tf
+++ infra/modules/curi/api_gateway/main.tf
@@ -53,7 +53,12 @@ resource "aws_apigatewayv2_integration" "example" {
 
 resource "aws_apigatewayv2_route" "example" {
   api_id    = aws_apigatewayv2_api.example.id
-  route_key = "POST /example/"
+  route_key = "POST /example/{proxy+}"
 
   target = "integrations/${aws_apigatewayv2_integration.example.id}"
+}
+
+resource "aws_apigatewayv2_stage" "example" {
+  api_id = aws_apigatewayv2_api.example.id
+  name   = "${terraform.workspace}-example-stage"
 }
