1fbc94962288b9c71759d3031bfe2440b40b5fdf
Luci Pak <pak.luci.13@gmail.com>
2021-09-08 18:25:15 -0700

    added aurora module

---
+++ infra/modules/curi/aurora_db/main.tf
@@ -0,0 +1,69 @@
+locals {
+  name   = "${terraform.workspace}-mantarray-db"
+  region = "us-east-1"
+  tags = {
+    Application = "mantarray-db"
+    Environment = terraform.workspace
+  }
+}
+provider "aws" {
+  region = local.region
+}
+
+resource "random_password" "master_password" {
+  length  = 10
+  special = false
+}
+resource "aws_db_parameter_group" "parameter_group" {
+  name        = "${local.name}-parameter-group"
+  family      = "aurora-mysql5.7"
+  description = "${local.name}-parameter-group"
+  tags        = local.tags
+}
+resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
+  name        = "${local.name}-cluster-parameter-group"
+  family      = "aurora-mysql5.7"
+  description = "${local.name}-cluster-parameter-group"
+  tags        = local.tags
+}
+
+# Providing a reference to our default VPC
+resource "aws_default_vpc" "default_vpc" {
+}
+
+# Providing a reference to our default subnets
+resource "aws_default_subnet" "default_subnet_a" {
+  availability_zone = "us-east-1a"
+}
+
+resource "aws_default_subnet" "default_subnet_b" {
+  availability_zone = "us-east-1b"
+}
+
+resource "aws_default_subnet" "default_subnet_c" {
+  availability_zone = "us-east-1c"
+}
+
+module "db" {
+  source = "terraform-aws-modules/rds-aurora/aws"
+
+  name           = local.name
+  engine         = "aurora-mysql"
+  engine_version = "5.7.mysql_aurora.2.09.2"
+  instance_type  = var.instance_type
+
+  subnets = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
+  vpc_id  = aws_default_vpc.default_vpc.id
+
+  replica_count     = 1
+  apply_immediately = true
+
+  username               = var.master_username
+  password               = random_password.master_password.result
+  create_random_password = false
+
+  db_parameter_group_name         = aws_db_parameter_group.parameter_group.id
+  db_cluster_parameter_group_name = aws_rds_cluster_parameter_group.cluster_parameter_group.id
+
+  tags = local.tags
+}
