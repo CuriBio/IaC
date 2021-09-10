c5c2055c8a10c2ee1d430538de6530c54f0ef9d3
Luci Pak <pak.luci.13@gmail.com>
2021-09-10 09:04:55 -0700

    added wildcards to policy

--- infra/modules/curi/aurora_db/iam.tf
+++ infra/modules/curi/aurora_db/iam.tf
@@ -12,12 +12,12 @@ data "aws_iam_policy_document" "assume_role_policy" {
     effect  = "Allow"
     principals {
       type        = "AWS"
-      identifiers = ["arn:aws:rds-db:us-east-1:077346344852:dbuser:cluster-CHFYU2HG6ZHXEYTJK5CWZZFRWQ/*"]
+      identifiers = ["arn:aws:rds-db:us-east-1:077346344852:dbuser:*/*"]
     }
   }
 }
 
 resource "aws_iam_role_policy_attachment" "dbconnect_policy" {
   role       = aws_iam_role.dbConnectRole.name
-  policy_arn = "arn:aws:rds-db:us-east-1:077346344852:dbuser:cluster-CHFYU2HG6ZHXEYTJK5CWZZFRWQ/*"
+  policy_arn = "arn:aws:rds-db:us-east-1:077346344852:dbuser:*/*"
 }
