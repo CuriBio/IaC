c16234a7c403bd3511f73de40b12327d48eab858
Luci Pak <pak.luci.13@gmail.com>
2021-09-10 08:13:09 -0700

    added iam role for db connection

---
+++ infra/modules/curi/aurora_db/iam.tf
@@ -0,0 +1,20 @@
+resource "aws_iam_role" "dbConnectRole" {
+  name               = "${terraform.workspace}-db-connect-role"
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags = {
+    Name = "${terraform.workspace}-db-iam-role"
+  }
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    actions   = ["rds-db:connect"]
+    effect    = "Allow"
+    resources = ["arn:aws:rds-db:us-east-1:077346344852:dbuser:cluster-CHFYU2HG6ZHXEYTJK5CWZZFRWQ/*"]
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "dbconnect_policy" {
+  role       = aws_iam_role.dbConnectRole.name
+  policy_arn = "arn:aws:rds-db:us-east-1:077346344852:dbuser:cluster-CHFYU2HG6ZHXEYTJK5CWZZFRWQ/*"
+}
