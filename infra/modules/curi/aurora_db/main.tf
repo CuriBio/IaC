0d6e89969b43b8301c95cb2c5a267ea111c39b26
Luci Pak <pak.luci.13@gmail.com>
2021-09-10 10:19:17 -0700

    calling unique security group

--- infra/modules/curi/aurora_db/main.tf
+++ infra/modules/curi/aurora_db/main.tf
@@ -23,8 +23,7 @@ resource "aws_rds_cluster_parameter_group" "cluster_parameter_group" {
 }
 
 # Providing a reference to our default VPC
-resource "aws_default_vpc" "default_vpc" {
-}
+resource "aws_default_vpc" "default_vpc" {}
 
 # Providing a reference to our default subnets
 resource "aws_default_subnet" "default_subnet_a" {
@@ -38,7 +37,31 @@ resource "aws_default_subnet" "default_subnet_b" {
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c"
 }
+resource "aws_security_group" "rds" {
+  description = "Allow inbound traffic from the security groups"
+  vpc_id      = aws_default_vpc.default_vpc.id
+}
 
+
+resource "aws_security_group_rule" "ingress_cidr_blocks" {
+  description       = "Allow inbound traffic from CIDR blocks"
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = [aws_default_vpc.default_vpc.cidr_block]
+  security_group_id = aws_security_group.rds.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  description       = "Allow all egress traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds.id
+}
 module "db" {
   source = "terraform-aws-modules/rds-aurora/aws"
 
@@ -47,10 +70,9 @@ module "db" {
   engine_version = "5.7.mysql_aurora.2.09.2"
   instance_type  = var.instance_type
 
-  subnets                             = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
-  vpc_id                              = aws_default_vpc.default_vpc.id
-  iam_database_authentication_enabled = true
-  iam_roles                           = [aws_iam_role.dbConnectRole.assume_role_policy]
+  subnets                 = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id, aws_default_subnet.default_subnet_c.id]
+  vpc_id                  = aws_default_vpc.default_vpc.id
+  allowed_security_groups = [aws_security_group.rds.id]
 
   replica_count     = 1
   apply_immediately = true
