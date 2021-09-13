9eec70e4ebb4fc5d44ff37c7f02b90fc6ee66ed7
Luci Pak <pak.luci.13@gmail.com>
2021-09-13 12:41:59 -0700

    updating VPC security group ids

--- infra/modules/curi/aurora_db/main.tf
+++ infra/modules/curi/aurora_db/main.tf
@@ -37,6 +37,31 @@ resource "aws_default_subnet" "default_subnet_b" {
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c"
 }
+resource "aws_security_group" "rds" {
+  description = "Allow inbound traffic from the security groups"
+  vpc_id      = aws_default_vpc.default_vpc.id
+}
+
+
+resource "aws_security_group_rule" "ingress_cidr_blocks" {
+  description       = "Allow all inbound traffic"
+  type              = "ingress"
+  from_port         = 3306
+  to_port           = 3306
+  protocol          = "tcp"
+  cidr_blocks       = aws_default_vpc.default_vpc.cidr_block
+  security_group_id = aws_security_group.rds.id
+}
+
+resource "aws_security_group_rule" "egress" {
+  description       = "Allow all egress traffic"
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.rds.id
+}
 
 module "db" {
   source = "terraform-aws-modules/rds-aurora/aws"
@@ -46,10 +71,11 @@ module "db" {
   engine_version = "5.7.mysql_aurora.2.09.2"
   instance_type  = var.instance_type
 
-  subnets = [aws_default_subnet.default_subnet_a.id]
+  subnets = [aws_default_subnet.default_subnet_a.id, aws_default_subnet.default_subnet_b.id]
   vpc_id  = aws_default_vpc.default_vpc.id
+  vpc_security_group_ids = [aws_default_vpc.default_vpc.default_security_group_id]
 
-  replica_count       = 1
+  replica_count       = 2
   apply_immediately   = true
   skip_final_snapshot = true
 
