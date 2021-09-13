098323301170b9593f1617b98252903ddc967815
Luci Pak <pak.luci.13@gmail.com>
2021-09-13 12:14:30 -0700

    removing security resource

--- infra/modules/curi/aurora_db/main.tf
+++ infra/modules/curi/aurora_db/main.tf
@@ -37,31 +37,7 @@ resource "aws_default_subnet" "default_subnet_b" {
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c"
 }
-resource "aws_security_group" "rds" {
-  description = "Allow inbound traffic from the security groups"
-  vpc_id      = aws_default_vpc.default_vpc.id
-}
 
-
-resource "aws_security_group_rule" "ingress_cidr_blocks" {
-  description       = "Allow all inbound traffic"
-  type              = "ingress"
-  from_port         = 3306
-  to_port           = 3306
-  protocol          = "tcp"
-  cidr_blocks       = aws_default_vpc.default_vpc.cidr_block
-  security_group_id = aws_security_group.rds.id
-}
-
-resource "aws_security_group_rule" "egress" {
-  description       = "Allow all egress traffic"
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.rds.id
-}
 module "db" {
   source = "terraform-aws-modules/rds-aurora/aws"
 
