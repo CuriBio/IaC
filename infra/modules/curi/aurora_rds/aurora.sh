b3135c627f4e2c1cb7a34fb0e24497b3e2c8a4f1
Luci Pak <pak.luci.13@gmail.com>
2021-09-15 10:07:22 -0700

    removed remote exec from ec2

--- infra/modules/curi/aurora_rds/aurora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo yum update -y
-sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm
-sudo yum install mysql-community-server
-sudo systemctl enable mysqld
-sudo systemctl start mysqld
-mysql -u $1 -p$2 -h $3 -P $4 < /tmp/schema.sql;
--- infra/modules/curi/aurora_rds/ec2.tf
+++ infra/modules/curi/aurora_rds/ec2.tf
@@ -22,26 +22,3 @@ resource "aws_instance" "ec2" {
 
 }
 
-resource "null_resource" "ssh_ec2_connection" {
-  depends_on = [aws_instance.ec2, tls_private_key.test_db_key, module.rds]
-  connection {
-    type        = "ssh"
-    host        = aws_instance.ec2.public_ip
-    user        = "ec2-user"
-    port        = "22"
-    private_key = tls_private_key.test_db_key.private_key_pem
-    agent       = false
-    timeout     = "60s"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo yum update -y",
-      "sudo rpm -Uvh https://dev.mysql.com/get/mysql57-community-release-el7-11.noarch.rpm",
-      "sudo yum install mysql-community-server",
-      "sudo systemctl enable mysqld",
-      "sudo systemctl start mysqld",
-      "mysql -u ${format(var.db_username)} -p${format(var.db_password)} -h ${module.rds.rds_cluster_instance_endpoints[0]} -P ${module.rds.rds_cluster_port} -e 'CREATE DATABASE test_db;'",
-    ]
-  }
-}
