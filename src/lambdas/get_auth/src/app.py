9d40dd76983e0ccff1ee394294e901855b42fe39
Tanner Peterson <tannermp23@hotmail.com>
2021-09-16 15:06:15 -0700

    added logging to get_auth lambda

--- src/lambdas/get_auth/src/app.py
+++ src/lambdas/get_auth/src/app.py
@@ -4,7 +4,7 @@ import os
 import sys
 
 import boto3
-
+from botocore.exceptions import ClientError
 
 # remove AWS pre-config that interferes with custom config
 root = logging.getLogger()
@@ -18,7 +18,6 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-# TODO add logging statements
 def get_tokens(username: str, password: str):
     client = boto3.client("cognito-idp")
     response = client.initiate_auth(
@@ -26,6 +25,7 @@ def get_tokens(username: str, password: str):
         AuthParameters={"USERNAME": username, "PASSWORD": password},
         ClientId=os.getenv("COGNITO_USER_POOL_CLIENT_ID"),
     )
+    logger.info(f"initiate_auth response: {response}")
 
     # response = {
     #     "ChallengeName": "SMS_MFA"|"SOFTWARE_TOKEN_MFA"|"SELECT_MFA_TYPE"|"MFA_SETUP"|"PASSWORD_VERIFIER"|"CUSTOM_CHALLENGE"|"DEVICE_SRP_AUTH"|"DEVICE_PASSWORD_VERIFIER"|"ADMIN_NO_SRP_AUTH"|"NEW_PASSWORD_REQUIRED",
@@ -55,13 +55,13 @@ def get_tokens(username: str, password: str):
 
 
 def handler(event, context):
+    logger.info(f"event: {event}")
     event_body = json.loads(event["body"])
     try:
         token_dict = get_tokens(event_body["username"], event_body["password"])
-    except:  # noqa
-        return {
-            "statusCode": 401,
-        }
+    except ClientError as e:
+        logger.info(f"Error: {e}")
+        return {"statusCode": 401}
 
     return {
         "statusCode": 200,
