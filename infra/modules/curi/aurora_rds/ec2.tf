a06684432f8e0fbbe50aa7f0e2ab0cad1161c6d0
Luci Pak <pak.luci.13@gmail.com>
2021-09-16 09:57:09 -0700

    fixed variable key

--- infra/modules/curi/aurora_rds/ec2.tf
+++ infra/modules/curi/aurora_rds/ec2.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "ec2" {
   ami                         = "ami-087c17d1fe0178315"
   associate_public_ip_address = true
   instance_type               = "t2.micro"
-  key_name                    = aws_key_pair.db_key_pair.name
+  key_name                    = aws_key_pair.db_key_pair.key_name
   subnet_id                   = aws_default_subnet.default_subnet_a.id
 
   vpc_security_group_ids = [aws_default_vpc.default_vpc.default_security_group_id]
