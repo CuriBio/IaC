7a6318494a0c323da393e0efd39e56334be95afa
Luci Pak <pak.luci.13@gmail.com>
2021-09-16 12:28:46 -0700

    testing all permissions

--- infra/modules/curi/aurora_rds/main.tf
+++ infra/modules/curi/aurora_rds/main.tf
@@ -26,7 +26,7 @@ data "aws_secretsmanager_secret" "db_secret" {
 }
 
 data "aws_secretsmanager_secret_version" "db_creds" {
-  secret_id = "arn:aws:secretsmanager:us-east-1:077346344852:secret:db-creds-WszNCl"
+  secret_id = data.aws_secretsmanager_secret.db_secret.arn
 }
 
 module "rds" {
