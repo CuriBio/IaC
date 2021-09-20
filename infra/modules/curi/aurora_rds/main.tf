f9989b4def4589e68c49c81f1a3cd88f6bc52ec8
Luci Pak <pak.luci.13@gmail.com>
2021-09-20 13:30:29 -0700

    specifying db name in action

--- infra/modules/curi/aurora_rds/main.tf
+++ infra/modules/curi/aurora_rds/main.tf
@@ -5,7 +5,7 @@ locals {
   )
   tags = {
     Application = "mantarray-db"
-    Environment = terraform.workspace
+    Identifier = terraform.workspace
   }
 }
 resource "aws_db_parameter_group" "parameter_group" {
