4b6b3f31a9337a723c2799c4e49248c70664655b
Luci Pak <pak.luci.13@gmail.com>
2021-09-28 11:04:36 -0700

    fixed sql syntax

--- .github/schema/schema.sql
+++ .github/schema/schema.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS mantarray_recordings;
 USE mantarray_recordings;
 
 CREATE TABLE IF NOT EXISTS s3_objects (
-  bucket_id int REFERENCES uploaded_s3_objects(id),
+  object_id int REFERENCES uploaded_s3_objects(id),
   stored_at datetime,
   kilobytes bigint,
   md5 varchar(255),
-  PRIMARY KEY (bucket_id),
+  PRIMARY KEY(object_id)
 );
 
 CREATE TABLE IF NOT EXISTS uploaded_s3_objects (
@@ -25,12 +25,12 @@ CREATE TABLE IF NOT EXISTS uploaded_s3_objects (
 
 CREATE TABLE IF NOT EXISTS mantarray_frontend_log_files (
   frontend_log_id varchar(255) PRIMARY KEY,
-  bucket_id int REFERENCES uploaded_s3_objects(id)
+  object_id int REFERENCES uploaded_s3_objects(id)
 );
 
 CREATE TABLE IF NOT EXISTS mantarray_backend_log_files (
   backend_log_id varchar(255) PRIMARY KEY,
-  bucket_id int REFERENCES uploaded_s3_objects(id),
+  object_id int REFERENCES uploaded_s3_objects(id),
   frontend_log_id varchar(255) REFERENCES mantarray_frontend_log_files(frontend_log_id),
   exit_code tinyint,
   software_version varchar(255),
@@ -43,12 +43,12 @@ CREATE TABLE IF NOT EXISTS mantarray_backend_log_files (
 
 
 CREATE TABLE IF NOT EXISTS mantarray_raw_files (
-  bucket_id int REFERENCES uploaded_s3_objects(id),
+  object_id int REFERENCES uploaded_s3_objects(id),
   well_index smallint,
   length_centimilliseconds int,
   recording_started_at datetime,
-  mantarray_recording_session_id varchar(255) REFERENCES mantarray_recording_sessions(mantarray_recording_session_id)
-  PRIMARY KEY (bucket_id)
+  mantarray_recording_session_id varchar(255) REFERENCES mantarray_recording_sessions(mantarray_recording_session_id),
+  PRIMARY KEY (object_id)
 );
 
 CREATE TABLE IF NOT EXISTS mantarray_recording_sessions (
