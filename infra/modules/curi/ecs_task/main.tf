04356abf60221d4d1d17baa346024081904e049b
Jason Person <jrperson@gmail.com>
2021-10-18 11:01:58 -0700

    merge development

--- infra/modules/curi/ecs_task/main.tf
+++ infra/modules/curi/ecs_task/main.tf
@@ -1,6 +1,6 @@
-resource "aws_iam_service_linked_role" "ecs_service_linked_role" {
-  aws_service_name = "ecs.amazonaws.com"
-}
+# resource "aws_iam_service_linked_role" "ecs_service_linked_role" {
+#   aws_service_name = "ecs.amazonaws.com"
+# }
 
 resource "aws_ecr_repository" "ecr" {
   name                 = "${terraform.workspace}-${var.image_name}"
