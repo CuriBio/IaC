2ce9efbe9caa830959a50ced4348402aff299ce0
Luci Pak <pak.luci.13@gmail.com>
2021-11-02 15:23:08 -0700

    added env vars

--- .github/scripts/aws_db_setup.sh
+++ .github/scripts/aws_db_setup.sh
@@ -3,7 +3,7 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region "us-east-1"
 
-SESSION=$(AWS_PAGER="" aws sts assume-role --region us-east-1 --role-arn $ROLE_ARN --role-session-name terraform --region us-east-1 --output json)
+SESSION=$(AWS_PAGER="" aws sts assume-role --region us-east-1 --role-arn $TEST_ROLE_ARN --role-session-name terraform --region us-east-1 --output json)
 unset AWS_PROFILE
 
 unset AWS_ACCESS_KEY_ID
@@ -15,14 +15,14 @@ export AWS_SECRET_ACCESS_KEY=$(echo $SESSION | jq -r ".Credentials.SecretAccessK
 unset AWS_SESSION_TOKEN
 export AWS_SESSION_TOKEN=$(echo $SESSION | jq -r ".Credentials.SessionToken")
 
-KEY=$( aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:077346344852:secret:db-ec2-key-pair-aofYm8  | jq --raw-output '.SecretString' )
-DB_PASSWORD=$( aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:077346344852:secret:db-creds-WszNCl  | jq --raw-output '.SecretString' | jq -r .password )
-DB_USERNAME=$( aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:077346344852:secret:db-creds-WszNCl  | jq --raw-output '.SecretString' | jq -r .username )
-DB_HOST=$( aws rds describe-db-instances --region=us-east-1 --db-instance-identifier "test-mantarray-rds-one" | jq --raw-output ".[][].Endpoint.Address" )
+KEY=$( aws secretsmanager get-secret-value --secret-id $DB_KEY_ARN | jq --raw-output '.SecretString' )
+DB_PASSWORD=$( aws secretsmanager get-secret-value --secret-id $DB_CREDS_ARN  | jq --raw-output '.SecretString' | jq -r .password )
+DB_USERNAME=$( aws secretsmanager get-secret-value --secret-id $DB_CREDS_ARN  | jq --raw-output '.SecretString' | jq -r .username )
+DB_HOST=$( aws rds describe-db-instances --region=us-east-1 --db-instance-identifier $DB_INSTANCE_ID | jq --raw-output ".[][].Endpoint.Address" )
 
 eval $(ssh-agent -s)
 ssh-agent -a /tmp/ssh_agent.sock > /dev/null
 ssh-add - <<< "${KEY}"
 
-scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./.github/schema/schema.sql ec2-user@ec2-54-226-191-183.compute-1.amazonaws.com:/tmp
-ssh -o StrictHostKeyChecking=no ec2-user@ec2-54-226-191-183.compute-1.amazonaws.com "mysql -u $DB_USERNAME -p$DB_PASSWORD -h $DB_HOST < /tmp/schema.sql"
+scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r ./.github/schema/schema.sql $EC2_HOST:/tmp
+ssh -o StrictHostKeyChecking=no $EC2_HOST "mysql -u $DB_USERNAME -p$DB_PASSWORD -h $DB_HOST < /tmp/schema.sql"
