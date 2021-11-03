eebfe5d84a65f7880d1418ad64f65c6b8bcbf57f
Jason Person <jrperson@gmail.com>
2021-11-03 14:06:22 -0700

    update

---
+++ infra/environments/outputs.tf
@@ -0,0 +1,24 @@
+output "role_arn" {
+  description = "terraform role arn"
+  value       = var.role_arn
+}
+
+output "db_creds_arn" {
+  description = "db credentials arn"
+  value       = var.db_creds_arn
+}
+
+output "db_name" {
+  description = "db name"
+  value       = module.aurora_database.db_name
+}
+
+output "jump_ec2_arn" {
+  description = "jump_ec2 arn"
+  value       = var.jump_ec2_arn
+}
+
+output "jump_host" {
+  description = "jump host"
+  value       = var.jump_host
+}
---
+++ infra/modules/curi/aurora_rds/outputs.tf
@@ -0,0 +1,4 @@
+output "db_name" {
+  description = "DB name"
+  value       = local.name
+}
