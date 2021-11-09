ba58b51000f14201ea2a9448fe54d43320c58b47
Luci Pak <pak.luci.13@gmail.com>
2021-11-09 08:57:16 -0800

    removed task

--- src/lambdas/sdk_analysis/src/lib/main.py
+++ src/lambdas/sdk_analysis/src/lib/main.py
@@ -47,11 +47,11 @@ def handle_db_metadata_insertions(bucket: str, key: str, db_host: str, args: lis
     """
 
     if not INFO_DICT:
-        set_info_dict(bucket, db_host)
+        set_info_dict()
 
     try:
         conn = pymysql.connect(
-            host=INFO_DICT["db_host"],
+            host=db_host,
             user=INFO_DICT["db_username"],
             passwd=INFO_DICT["db_password"],
             db=INFO_DICT["db_name"],
@@ -104,10 +104,9 @@ def handle_db_metadata_insertions(bucket: str, key: str, db_host: str, args: lis
     conn.commit()
 
 
-def set_info_dict(bucket: str, db_host: str):
+def set_info_dict():
     # Retrieve DB creds
     secrets = get_ssm_secrets()
     INFO_DICT["db_username"] = secrets["username"]
     INFO_DICT["db_password"] = secrets["password"]
-    INFO_DICT["db_host"] = db_host
     INFO_DICT["db_name"] = "mantarray_recordings"
