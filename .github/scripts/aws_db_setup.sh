93b11df39ff7effe2f2721639b766a243cb7e364
Luci Pak <luciypak@gmail.com>
2021-11-19 15:00:06 -0800

    first workflow check

--- .github/scripts/aws_db_setup.sh
+++ .github/scripts/aws_db_setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-TEST_ROLE_ARN=$1
+ASSUMED_ROLE_ARN=$1
 DB_KEY_ARN=$2
 DB_CREDS_ARN=$3
 DB_HOST=$4
@@ -9,7 +9,7 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
 aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
 aws configure set default.region "us-east-1"
 
-SESSION=$(AWS_PAGER="" aws sts assume-role --region us-east-1 --role-arn $TEST_ROLE_ARN --role-session-name terraform --region us-east-1 --output json)
+SESSION=$(AWS_PAGER="" aws sts assume-role --region us-east-1 --role-arn $ASSUMED_ROLE_ARN --role-session-name terraform --region us-east-1 --output json)
 unset AWS_PROFILE
 
 unset AWS_ACCESS_KEY_ID
