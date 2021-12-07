38c2442b466c45e9ba245c89d137ef1806ff0a16
Luci Pak <luciypak@gmail.com>
2021-12-07 14:22:58 -0600

    removed hardcoded workspace

--- .github/scripts/dump_prod_data.sh
+++ .github/scripts/dump_prod_data.sh
@@ -8,6 +8,7 @@ ssh-add - <<< "${KEY}"
 
 ssh -o StrictHostKeyChecking=no $EC2_HOST "mysqldump -f -u $DB_USERNAME -p$DB_PASSWORD -h $DB_HOST --add-drop-database --set-gtid-purged=OFF --databases mantarray_recordings > /tmp/data_dump.sql"
 scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null -r $EC2_HOST:/tmp/data_dump.sql /tmp
+ssh -o StrictHostKeyChecking=no $EC2_HOST "rm /tmp/data_dump.sql"
 
 eval $(ssh-agent -k)
 rm -rf /tmp/ssh_agent.sock
